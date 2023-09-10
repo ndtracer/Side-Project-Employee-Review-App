93bde234e564c6c7860a9323636919921635a4d1
ndtracer <tracejanota@gmail.com>
2023-09-09 21:50:20 -0500

    added files

---
+++ Scripts.js/main.js
@@ -0,0 +1,15 @@
+// Purpose: Manager Login(low-level)
+// Manager Chooses Employees
+//      Has option to Add/Remove Employees
+// Chosen Employee Page Brings up Criteria (Able to Add/Remove Criteria)
+// Input 1-5 Values for each Criteria
+// Optional Notes for Each Entry or Overall Comments
+// Submit and Store Input Values
+// Manager Page (or each Employee Page)Has option to Review
+// Set Time Period or individual day for review
+// Individual Day Output: Prints Saved Inputs for that day
+// Time Period Output: Averages Values over period of time and gives Overall Rating. 
+
+// function toggle() {
+//     let mytab = 
+// }
---
+++ Scripts.js/sel-page.js
@@ -0,0 +1,12 @@
+const btn1 = document.getElementById("wrkstbtn");
+const para = document.querySelector (h2)
+btn1.addEventListener("click", function() {
+    
+    para.classList.add("selpageh3");
+    
+})
+
+
+function revealDept() {
+    document.getElementsById ('depth2') 
+}
